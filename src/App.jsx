be5641ee52e7f5b635ae2101539fff1a4850cb05
Sahana G <sahanagopal23@gmail.com>
2023-09-01 09:46:31 +0530

    initial project update

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import Home from './Component/Home'
+import HomeClass from './Component/HomeClass'
+import About from './Component/About'
+import AboutClass from './Component/AboutClass'
+import RenderingComponent from './Component/RenderingComponent'
+import LifeCycleComponent from './Component/lifecyclecomponent'
+import DesignComponent from './Component/DesignComponent'
+
+function App() {
+  return (
+    <div>
+
+     <DesignComponent />
+     
+
+    </div>
+
+
+  )
+}
+
+
+export default App
